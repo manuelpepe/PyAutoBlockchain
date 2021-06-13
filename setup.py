5190afee2449504273a6b80bf8ac55f1a8373865
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-06-13 18:25:17 -0300

    fix include package data

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 
 setup(
     name='PyAutoBlockchain',
-    version='1.0',
+    version='1.0.3',
     description='PAB is a framework for developing and running custom tasks in crypto blockchains.',
     author='Manuel Pepe',
     author_email='manuelpepe-dev@outlook.com.ar',
     url = 'https://github.com/manuelpepe/PyAutoBlockchain',
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'pab = pab.cli:main',

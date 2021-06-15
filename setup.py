e83368ccdaa845da3c645c1cbfd7bb789a0ea25a
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-06-14 22:09:30 -0300

    Fix PyPi description

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 import setuptools
 
+from pathlib import Path
 from distutils.core import setup
+
+README = Path(__file__).parent / "README.md"
+with open(README, "r") as fp:
+    long_description = fp.read()
 
 setup(
     name='PyAutoBlockchain',
-    version='0.2',
+    version='0.2.1',
     description='PAB is a framework for developing and running custom tasks in crypto blockchains.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Manuel Pepe',
     author_email='manuelpepe-dev@outlook.com.ar',
     url = 'https://github.com/manuelpepe/PyAutoBlockchain',

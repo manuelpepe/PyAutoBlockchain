e7812fb1e5de20ded8ec5e78b55dea566d5b9d5d
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-06-15 17:32:21 -0300

    new transactionTimeout config

--- pab/transaction.py
+++ pab/transaction.py
@@ -3,8 +3,11 @@ import logging
 from web3 import Web3
 from hexbytes import HexBytes
 
+from pab.config import APP_CONFIG
 
-class TransactionError(Exception): pass
+
+class TransactionError(Exception): 
+    pass
 
 
 class TransactionHandler:
@@ -24,14 +27,14 @@ class TransactionHandler:
             "nonce" : self.w3.eth.getTransactionCount(self.owner),
         }
 
-    def transact(self, func: callable, args: tuple):
+    def transact(self, func: callable, args: tuple, timeout: int = APP_CONFIG.get("transactionTimeout", 200)):
         """ Submits transaction and prints hash """
         if not self.private_key:
             raise TransactionError("Private key not set")
         txn = func(*args).buildTransaction(self._trans_details())
         stxn = self.w3.eth.account.sign_transaction(txn, private_key=self.private_key)
         sent = self.w3.eth.send_raw_transaction(stxn.rawTransaction)
-        rcpt = self.w3.eth.wait_for_transaction_receipt(sent) 
+        rcpt = self.w3.eth.wait_for_transaction_receipt(sent, timeout=timeout) 
         self.logger.info(f"Block Hash: {rcpt.blockHash.hex()}")
         self.logger.info(f"Gas Used: {rcpt.gasUsed}")
         return sent, rcpt

c0e0e359a152dc82202431d870aba366328d5ea4
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-06-22 18:09:54 -0300

    fix upload pyp

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -7,6 +7,29 @@
 act="$1"
 env="$2"
 
+# Funcs
+check_if_changelog_is_wip () {
+    grep "\*\*\*WIP\*\*\*" CHANGELOG.md > /dev/null && echo "CHANGELOG.md is WIP" && exit 1 
+}
+
+confirm () {
+    echo -n "$1 (y/n) "
+    read "ans"
+    if [[ "${ans^^}" != "Y" ]]; then
+        echo "$2"; exit 1
+    fi
+}
+
+
+# Setup
+if [[ "$env" == "master" ]]; then
+    check_if_changelog_is_wip
+fi
+
+confirm "Have you upgraded versions?" "Please upgrade version"
+
+
+# Main
 if [[ "$act" == "upload" ]]; then
     rm -rf dist/*
     python3 setup.py sdist bdist_wheel

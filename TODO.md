65d73b9e40e6d9318561325446cf78c7bfb0ee70
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-11-04 20:44:42 -0300

    deprecated util functions

---
+++ TODO.md
@@ -0,0 +1,21 @@
+## Remove deprecated functions from pab.utils
+
+## `pab edit-config` fails if config.json is not found or is not valid json. Should create it.
+
+## Give more descriptive error when unexpected parameters are passed to tasks. E.g. traceback:
+```
+Traceback (most recent call last):
+  File "/mnt/d/Dev/ACSInfoGath/venv/bin/pab", line 33, in <module>
+    sys.exit(load_entry_point('PyAutoBlockchain', 'console_scripts', 'pab')())
+  File "/mnt/d/Dev/PyAutoBlockchain/pab/cli.py", line 136, in main
+    args.func(args, logger)
+  File "/mnt/d/Dev/PyAutoBlockchain/pab/cli.py", line 82, in run
+    queue = QueueLoader(blockchain).load()
+  File "/mnt/d/Dev/PyAutoBlockchain/pab/queue.py", line 118, in load
+    return self._create_queue_from_list(tasks)
+  File "/mnt/d/Dev/PyAutoBlockchain/pab/queue.py", line 129, in _create_queue_from_list
+    strat = self._create_strat_from_data(data)
+  File "/mnt/d/Dev/PyAutoBlockchain/pab/queue.py", line 137, in _create_strat_from_data
+    return strat_class(self.blockchain, data["name"], **data.get("params", {}))
+TypeError: __init__() got an unexpected keyword argument 'contract'
+```
--- pab/utils.py
+++ pab/utils.py
@@ -1,4 +1,5 @@
 import json
+import warnings
 
 from pathlib import Path
 from contextlib import contextmanager
@@ -16,19 +17,39 @@ class KeyfileOverrideException(Exception): pass
 UNIT_MULTIPLIER = 1000000000000000000
 
 
+def amountToDecimal(amount: int, decimals: int = 18) -> float:
+    return amount / 10 ** decimals
+
+
 def amountToPZAP(amount: int) -> str:
+    warnings.warn(
+        "amountToPZAP will be deprecated in version 0.4, use amountToDecimal instead",
+        PendingDeprecationWarning
+    )
     return f"{amount / UNIT_MULTIPLIER:.8f}"
 
 
 def amountToWBTC(amount: int) -> str:
+    warnings.warn(
+        "amountToWBTC will be deprecated in version 0.4, use amountToDecimal instead",
+        PendingDeprecationWarning
+    )
     return f"{amount / 100000000:.8f}"
 
 
 def amountToLPs(amount: int) -> str:
+    warnings.warn(
+        "amountToLPs will be deprecated in version 0.4, use amountToDecimal instead",
+        PendingDeprecationWarning
+    )
     return f"{amount / UNIT_MULTIPLIER:.25f}"
 
 
 def PZAPToAmount(pzap: str) -> int:
+    warnings.warn(
+        "PZAPToAmount will be deprecated in version 0.4, use amountToDecimal instead",
+        PendingDeprecationWarning
+    )
     return int(float(pzap) * UNIT_MULTIPLIER)
 
 

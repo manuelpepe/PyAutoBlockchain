d062c2333ee26213d6e39cd2e3333e19fb07d20f
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-11-12 02:52:32 -0300

    removed unnecessary check from tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,15 +8,7 @@ TESTS_WORKING_DIR = Path(__file__).parent / "resources"
 os.chdir(TESTS_WORKING_DIR)
 sys.path.append(str(TESTS_WORKING_DIR))
 
-from pab.exceptions import MissingConfig
-
-try:
-    from pab.config import APP_CONFIG
-    APP_CONFIG.get('endpoint')
-except MissingConfig as err:
-    print("ERROR: Config not found. Create config with valid rpc to run tests.")
-    raise err
-
+from pab.config import APP_CONFIG
 from pab.blockchain import Blockchain
 from pab.queue import QueueLoader
 

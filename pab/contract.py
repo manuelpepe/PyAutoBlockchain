b5880916ef417b3a8c50fa7bdbe3d59b3499db2e
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-12-16 00:33:41 -0300

    contracts.json validations

--- pab/contract.py
+++ pab/contract.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 if typing.TYPE_CHECKING:
     from web3 import Web3
+    from web3.contract import Contract
 
 from pab.config import ABIS_DIR, CONTRACTS_FILE
 
@@ -19,14 +20,33 @@ class ContractManager:
         self.w3 = w3
         self.root = root
         self.abisdir = root / ABIS_DIR
-        self.contracts = self.load_contracts()
+        self.contracts = self._load_contracts()
     
-    def load_contracts(self):
+    def _load_contracts(self) -> list['Contract']:
+        """ Reads `contracts.json` and checks format. """
         with open(self.root / CONTRACTS_FILE, "r") as fp:
-            self.contracts = json.load(fp)
-        return self.contracts
+            contracts = json.load(fp)
+            if not isinstance(contracts, dict):
+                raise ContractDefinitionError("Data in contracts.json is not a dict")
+            for name, data in contracts.items():
+                self._check_valid_contract_data(data)
+                self._check_abifile_exists(data['abifile'])
+        return contracts
+    
+    def _check_valid_contract_data(self, data: dict):
+        if not isinstance(data, dict):
+            raise ContractDefinitionError("Contract data must be a dict with address and abifile.")
+        req_fields = ['address', 'abifile']
+        for attr in req_fields:
+            if not attr in data.keys():
+                raise ContractDefinitionError("Contract data must be a dict with address and abifile.")
 
-    def get(self, name: str):
+    def _check_abifile_exists(self, abifile):
+        filepath = Path(self.abisdir / abifile)
+        if not filepath.is_file():
+            raise ContractDefinitionError(f"ABI file at abis/{abifile} not found.")
+
+    def get(self, name: str) -> 'Contract':
         from web3 import Web3
         if not name in self.contracts.keys():
             raise ValueError("Contract not found.")
@@ -37,3 +57,6 @@ class ContractManager:
                 abi=fp.read()
             )
 
+
+class ContractDefinitionError(Exception):
+    ...

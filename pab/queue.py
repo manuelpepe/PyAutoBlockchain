d5243e66519cc24e057f330533fb44ada5841386
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2021-12-17 03:11:45 -0300

    typehints and tasks validations

--- pab/queue.py
+++ pab/queue.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-from typing import Optional, List
+from typing import Any, Callable, Optional, List
 from pathlib import Path
 from datetime import datetime, timedelta
 
@@ -25,19 +25,21 @@ class QueueItem:
         self.repeat_every = repeat_every
         self.last_start = 0
 
-    def reschedule(self):
+    def reschedule(self) -> None:
+        """ Calculates next execution from repetition data. """
         next_run = self.next_repetition_time() if self.repeats() else self.RUN_NEVER
         self.schedule_for(next_run)
 
-    def repeats(self):
+    def repeats(self) -> bool:
+        """ True if """
         return bool(self.repeat_every)
 
-    def next_repetition_time(self):
+    def next_repetition_time(self) -> int:
         last = datetime.fromtimestamp(self.last_start)
         new = last + timedelta(**self.repeat_every)
         return int(new.timestamp())
 
-    def schedule_for(self, next_at: int):
+    def schedule_for(self, next_at: int) -> None:
         if type(next_at) != int:
             raise ValueError("Schedule for must receive an integer timestamp")
         if next_at == self.RUN_NEVER:
@@ -47,7 +49,7 @@ class QueueItem:
             self.logger.info(f"Next run of {self} will be at {timestamp}")
         self.next_at = next_at
     
-    def is_ready(self):
+    def is_ready(self) -> bool:
         if self.next_at == self.RUN_ASAP:
             return True
         elif self.next_at == self.RUN_NEVER:
@@ -57,7 +59,7 @@ class QueueItem:
         else:
             raise ValueError(f"Wrong value {self.next_at} of type {type(self.next_at)} for QueuedItem.next_at #{self.id}")
 
-    def process(self):
+    def process(self) -> None:
         try:
             self._process()
         except SpecificTimeRescheduleError as err:
@@ -67,7 +69,7 @@ class QueueItem:
             self.logger.warning(err)
             self.reschedule()
 
-    def _process(self):
+    def _process(self) -> None:
         if self.is_ready():
             self.logger.info(f"Running task {self.strategy}")
             self.last_start = datetime.now().timestamp()
@@ -103,18 +105,27 @@ class QueueLoader:
         self.blockchain = blockchain
         self.strategies = strategies
 
-    def load(self):
+    def load(self) -> Queue:
         with open(Path.cwd() / TASKS_FILE, "r") as fp:
             tasks = self._load_tasks_json_or_exception(fp)
             return self._create_queue_from_list(tasks)
         
-    def _load_tasks_json_or_exception(self, fhandle):
+    def _load_tasks_json_or_exception(self, fhandle) -> List[dict]:
         try:
-            return json.load(fhandle)
+            data = json.load(fhandle)
+            self._validate_raw_data(data)
+            return data
         except json.JSONDecodeError as err:
             raise QueueLoadError("Error loading tasks.json") from err
 
-    def _create_queue_from_list(self, tasks: List[dict]):
+    def _validate_raw_data(self, data: Any) -> None:
+        if not isinstance(data, list):
+            raise QueueLoadError("Data should be a list of dicts")
+        for item in data:
+            if not isinstance(item, dict):
+                raise QueueLoadError("Data should be a list of dicts")
+
+    def _create_queue_from_list(self, tasks: List[dict]) -> Queue:
         out = []
         for ix, data in enumerate(tasks):
             strat = self._create_strat_from_data(data)
@@ -127,7 +138,7 @@ class QueueLoader:
         strat_class = self._find_strat_by_name(data["strategy"])
         return strat_class(self.blockchain, data["name"], **data.get("params", {}))
 
-    def _find_strat_by_name(self, name: str):
+    def _find_strat_by_name(self, name: str) -> Callable | None:
         for class_ in self.strategies:
             if class_.__name__ == name:
                 return class_
